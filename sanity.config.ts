e0a3326dbf8ba1b83e0b9c1ff34153c6a85b5d8b
pleasehelpm32 <joshua543210@hotmail.com>
2025-11-17 12:18:18 -0500

    Remove vision tool from Sanity config to fix build error
    - Remove structureTool and visionTool imports
    - Keep only schema definition (sufficient for Next.js consumer app)
    - Fixes missing @sanity/vision dependency error

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,4 @@
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './sanity/schemaTypes'
 
 export default defineConfig({
@@ -9,11 +7,6 @@ export default defineConfig({
 
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-
-  plugins: [
-    structureTool(),
-    visionTool(),
-  ],
 
   schema: {
     types: schemaTypes,
